0424fc2325a0ab1e2129af1817947048f3f325f8
protsenkoiv <protsenkoiv@400D-1>
2022-09-21 15:55:55 +0700

    Добавлена модель представления

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ViewModels
+{
+   public class MainViewModel:ViewModelBase
+    {
+        private string display = "lopata";
+        public string Display
+        {
+            get => display;
+            private set
+            {
+                display = value;
+                OnPropertyChanged("Display");
+            }
+        }
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(String name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+    
+}
